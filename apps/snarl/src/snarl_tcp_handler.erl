4eb19cb1d7205f9467abdab81ef3d9aaf2fd76be
Heinz N. Gies <heinz@licenser.net>
2013-02-25 01:02:54 +0100

    Fixed bug caused by update of mdns server lib.

--- apps/snarl/src/snarl_tcp_handler.erl
+++ apps/snarl/src/snarl_tcp_handler.erl
@@ -4,12 +4,14 @@
 
 -include("snarl_version.hrl").
 
--export([init/1, message/2]).
+-export([init/2, message/2]).
 
--ignore_xref([init/1, message/2]).
+-ignore_xref([init/2, message/2]).
 
-init([]) ->
-    {ok, stateless}.
+-record(state, {port}).
+
+init(Prot, []) ->
+    {ok, #state{port = Prot}}.
 
 %%%===================================================================
 %%% User Functions
@@ -18,15 +20,15 @@ init([]) ->
 -spec message(fifo:smarl_message(), term()) -> any().
 
 message(version, State) ->
-    {reply, ?VERSION, State};
+    {stop, normal, ?VERSION, State};
 
 message({user, list}, State) ->
-    {reply, snarl_user:list(), State};
+    {stop, normal, snarl_user:list(), State};
 
 message({user, get, {token, Token}}, State) ->
     case snarl_token:get(Token) of
         {ok, not_found} ->
-            {reply, not_found, State};
+            {stop, normal, not_found, State};
         {ok, User} ->
             message({user, get, User}, State)
     end;
@@ -34,41 +36,41 @@ message({user, get, {token, Token}}, State) ->
 message({user, cache, {token, Token}}, State) ->
     case snarl_token:get(Token) of
         {ok, not_found} ->
-            {reply, not_found, State};
+            {stop, normal, not_found, State};
         {ok, User} ->
             message({user, cache, User}, State)
     end;
 
 message({user, get, User}, State) when
       is_binary(User) ->
-    {reply,
+    {stop, normal,
      snarl_user:get(User),
      State};
 
 message({user, set, User, Attribute, Value}, State) when
       is_binary(User) ->
-    {reply,
+    {stop, normal,
      snarl_user:set(User, Attribute, Value),
      State};
 
 message({user, set, User, Attributes}, State) when
       is_binary(User) ->
-    {reply,
+    {stop, normal,
      snarl_user:set(User, Attributes),
      State};
 
 message({user, lookup, User}, State) when is_binary(User) ->
-    {reply,
+    {stop, normal,
      snarl_user:lookup(User),
      State};
 
 message({user, cache, User}, State) ->
-    {reply,
+    {stop, normal,
      snarl_user:cache(ensure_binary(User)),
      State};
 
 message({user, add, User}, State) ->
-    {reply,
+    {stop, normal,
      snarl_user:add(ensure_binary(User)),
      State};
 
@@ -82,53 +84,53 @@ message({user, auth, User, Pass}, State) ->
                   {ok, Token} = snarl_token:add(UUID),
                   {ok, {token, Token}}
           end,
-    {reply,
+    {stop, normal,
      Res,
      State};
 
 message({user, allowed, {token, Token}, Permission}, State) ->
     case snarl_token:get(Token) of
         {ok, not_found} ->
-            {reply, false, State};
+            {stop, normal, false, State};
         {ok, User} ->
-            {reply,
+            {stop, normal,
              snarl_user:allowed(User, Permission),
              State}
     end;
 
 message({user, allowed, User, Permission}, State) ->
-    {reply,
+    {stop, normal,
      snarl_user:allowed(ensure_binary(User), Permission),
      State};
 
 message({user, delete, User}, State) ->
-    {reply,
+    {stop, normal,
      snarl_user:delete(ensure_binary(User)),
      State};
 
 message({user, passwd, User, Pass}, State) ->
-    {reply,
+    {stop, normal,
      snarl_user:passwd(ensure_binary(User), ensure_binary(Pass)),
      State};
 
 message({user, join, User, Group}, State) ->
-    {reply, snarl_user:join(ensure_binary(User), Group), State};
+    {stop, normal, snarl_user:join(ensure_binary(User), Group), State};
 
 message({user, leave, User, Group}, State) ->
-    {reply, snarl_user:leave(ensure_binary(User), Group), State};
+    {stop, normal, snarl_user:leave(ensure_binary(User), Group), State};
 
 message({user, grant, User, Permission}, State) ->
-    {reply, snarl_user:grant(ensure_binary(User), Permission), State};
+    {stop, normal, snarl_user:grant(ensure_binary(User), Permission), State};
 
 message({user, revoke, User, Permission}, State) ->
-    {reply, snarl_user:revoke(ensure_binary(User), Permission), State};
+    {stop, normal, snarl_user:revoke(ensure_binary(User), Permission), State};
 
 message({user, revoke_all, User, Permission}, State) ->
-    {reply, snarl_user:revoke_all(ensure_binary(User), Permission), State};
+    {stop, normal, snarl_user:revoke_all(ensure_binary(User), Permission), State};
 
 message({token, delete, Token}, State) when
       is_binary(Token) ->
-    {reply, snarl_token:delete(Token), State};
+    {stop, normal, snarl_token:delete(Token), State};
 
 
 %%%===================================================================
@@ -136,56 +138,56 @@ message({token, delete, Token}, State) when
 %%%===================================================================
 
 message({user, set_resource, User, Resource, Value}, State) ->
-    {reply, snarl_user:set_resource(ensure_binary(User), Resource, Value), State};
+    {stop, normal, snarl_user:set_resource(ensure_binary(User), Resource, Value), State};
 
                                                 %message({user, get_resource, User, Resource}, State) ->
-                                                %    {reply, snarl_user:get_resource(ensure_binary(User), Resource), State};
+                                                %    {stop, normal, snarl_user:get_resource(ensure_binary(User), Resource), State};
 
 message({user, claim_resource, User, Resource, Ammount}, State) ->
     ID = uuid:uuid4(),
-    {reply, {ID, snarl_user:claim_resource(ensure_binary(User), ID, Resource, Ammount)}, State};
+    {stop, normal, {ID, snarl_user:claim_resource(ensure_binary(User), ID, Resource, Ammount)}, State};
 
 message({user, free_resource, User, Resource, ID}, State) ->
-    {reply, snarl_user:free_resource(ensure_binary(User), Resource, ID), State};
+    {stop, normal, snarl_user:free_resource(ensure_binary(User), Resource, ID), State};
 
 message({user, resource_stat, User}, State) ->
-    {reply, snarl_user:get_resource_stat(ensure_binary(User)), State};
+    {stop, normal, snarl_user:get_resource_stat(ensure_binary(User)), State};
 
 %%%===================================================================
 %%% Group Functions
 %%%===================================================================
 
 message({group, list}, State) ->
-    {reply, snarl_group:list(), State};
+    {stop, normal, snarl_group:list(), State};
 
 message({group, get, Group}, State) ->
-    {reply, snarl_group:get(Group), State};
+    {stop, normal, snarl_group:get(Group), State};
 
 message({group, set, Group, Attribute, Value}, State) when
       is_binary(Group) ->
-    {reply,
+    {stop, normal,
      snarl_group:set(Group, Attribute, Value),
      State};
 
 message({group, set, Group, Attributes}, State) when
       is_binary(Group) ->
-    {reply,
+    {stop, normal,
      snarl_group:set(Group, Attributes),
      State};
 
 message({group, add, Group}, State) ->
-    {reply, snarl_group:add(Group), State};
+    {stop, normal, snarl_group:add(Group), State};
 
 message({group, delete, Group}, State) ->
-    {reply, snarl_group:delete(Group), State};
+    {stop, normal, snarl_group:delete(Group), State};
 
 message({group, grant, Group, Permission}, State) when
       is_binary(Group),
       is_list(Permission)->
-    {reply, snarl_group:grant(Group, Permission), State};
+    {stop, normal, snarl_group:grant(Group, Permission), State};
 
 message({group, revoke, Group, Permission}, State) ->
-    {reply, snarl_group:revoke(Group, Permission), State};
+    {stop, normal, snarl_group:revoke(Group, Permission), State};
 
 message(Message, State) ->
     io:format("Unsuppored 0MQ message: ~p", [Message]),
