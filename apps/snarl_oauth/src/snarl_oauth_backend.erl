e858f56e869f29d6f9742e8c0e05c1a0a7ce4689
Heinz N. Gies <heinz@licenser.net>
2015-08-09 23:55:42 +0200

    Changed handling of client identification.

--- apps/snarl_oauth/src/snarl_oauth_backend.erl
+++ apps/snarl_oauth/src/snarl_oauth_backend.erl
@@ -68,8 +68,6 @@ authenticate_user({Username, Password, OTP}, AppContext) ->
             {error, notfound}
     end.
 
-authenticate_client({UserID}, AppContext) ->
-    {ok, {AppContext, UserID}};
 authenticate_client({ClientId, ClientSecret},
                     AppContext = #oauth_state{realm = Realm}) ->
     case snarl_client:auth(Realm, ClientId, ClientSecret) of
@@ -77,7 +75,12 @@ authenticate_client({ClientId, ClientSecret},
             {ok, {AppContext, UserID}};
         not_found ->
             {error, notfound}
-    end.
+    end;
+authenticate_client(ClientID, AppContext) when is_binary(ClientID) ->
+    {ok, C} = libsnarl_client:lookup(ClientID),
+    UUID = ft_client:uuid(C),
+    {ok, {AppContext, UUID}}.
+
 
 %% Is this a Authrorization Code?
 associate_access_code(AccessCode, Context, AppContext) ->
