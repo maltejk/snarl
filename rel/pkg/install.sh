9dceb26b83b69fce6f9f16e9b5f7199606c835bd
Heinz N. Gies <heinz@licenser.net>
2015-10-08 10:37:40 +0200

    Fully remove config update scirpt

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -10,13 +10,6 @@ BACKUP_FILE=/var/db/snarl/backup_$DBID.tar.gz
 
 case $2 in
     PRE-INSTALL)
-        #if grep '^Image: base64 1[43].[1234].*$' /etc/product
-        #then
-        #    echo "Image version supported"
-        #else
-        #    echo "This image version is not supported please use the base64 13.2.1 image or later."
-        #    exit 1
-        #fi
         if grep "^$GROUP:" /etc/group > /dev/null 2>&1
         then
             echo "Group already exists, skipping creation."
@@ -64,10 +57,10 @@ case $2 in
             cp ${CONFFILE}.example ${CONFFILE}
             $SED -i bak -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
         else
-            echo "Merging old file with new template, the original can be found in ${CONFFILE}.old."
-            /opt/local/fifo-snarl/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
-                mv ${CONFFILE} ${CONFFILE}.old &&
-                mv ${CONFFILE}.new ${CONFFILE}
+            echo "Please make sure you update your config according to the update manual!"
+            #/opt/local/fifo-sniffle/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
+            #    mv ${CONFFILE} ${CONFFILE}.old &&
+            #    mv ${CONFFILE}.new ${CONFFILE}
         fi
         ;;
 esac
